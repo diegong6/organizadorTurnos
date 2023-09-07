0ead59e5f11f6f26508ed3b0f198bd8c3b6ec5ac
Diego Villegas <diegon.villegas@gmail.com>
2023-09-07 04:39:40 -0300

    cambie estetica de sombras

--- app.js
+++ app.js
@@ -47,9 +47,9 @@ function getTasks() {
     let description = tasks[i].description;
 
     tasksView.innerHTML += `<div class="card mb-3 rounded shadow-lg">
-        <div class="card-body">
+        <div class="card-body sombra-card">
           <p style="font-size: 17px; text-align: center; justify-content: center; color: var(--clr-dark); padding: 5px; ">${title} <i class="bi bi-arrow-right"></i> ${description}
-          <br><br><a href="#" onclick="deleteTask('${title}')" class="btn btn-danger ml-5 rounded">Borrar turno</a>
+          <br><br><a href="#" onclick="deleteTask('${title}')" class="btn btn-danger ml-5 rounded sombra-card">Borrar turno</a>
           </p>
         </div>
       </div>`;
